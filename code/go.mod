12f5c7ddcaaf29f628bcb1ec2b34121f0994f656
zel <isral888andrade@gmail.com>
2025-10-08 18:15:52 -0300

    web server setup

---
+++ code/go.mod
@@ -0,0 +1,3 @@
+module github.com/Israel-Andrade-P/Chirpy.git
+
+go 1.25.1
---
+++ code/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	fmt.Println("Starting server on port 8080...")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Error starting server. ERR: %v", err)
+	}
+}
