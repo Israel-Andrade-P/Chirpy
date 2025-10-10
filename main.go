3f7ac8e8c8aacbb394a70bc8e1e0449a89992525
zel <isral888andrade@gmail.com>
2025-10-10 16:31:43 -0300

    add some middlewares

--- main.go
+++ main.go
@@ -4,16 +4,44 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Counter reset.\n"))
+}
+
 func main() {
+	apicfg := &apiConfig{}
 	mux := http.NewServeMux()
 
 	fileServer := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	fileServer = noCacheFileServer(fileServer)
 
-	mux.Handle("/app/", fileServer)
-	mux.Handle("/app/assets/logo.png", fileServer)
-	mux.HandleFunc("/healthz", readiness)
+	mux.Handle("/app/", apicfg.middlewareMetricsInc(fileServer))
+	mux.Handle("/app/assets/logo.png", apicfg.middlewareMetricsInc(fileServer))
+	mux.HandleFunc("GET /healthz", readiness)
+	mux.HandleFunc("GET /metrics", apicfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apicfg.handlerReset)
 
 	port := "8080"
 	server := &http.Server{
@@ -32,4 +60,12 @@ func readiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func noCacheFileServer(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.Header.Del("If-Modified-Since")
+		r.Header.Del("If-None-Match")
+		h.ServeHTTP(w, r)
+	})
 }
